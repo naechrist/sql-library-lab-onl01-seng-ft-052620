a9fb7addb5a1e64fd8b1e5217f68eb6d74bffdda
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-24 15:53:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
