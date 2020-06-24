bfe8759efdf6ec5ae1916c65850224f2334d8a57
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-24 15:54:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,11 +8,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1"
+  ""
 end
 
 def select_series_title_with_most_human_characters
